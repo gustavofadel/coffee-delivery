c4a22e879d17f4570d4650506aef8867fa7c4b35
Gustavo Fadel <ghffadel@gmail.com>
2023-11-16 15:07:50 -0400

    chore: Configuring theme and styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  background: '#FAFAFA',
+
+  'base-button': '#E6E5E5',
+  'base-card': '#F3F2F2',
+  'base-hover': '#D7D5D5',
+  'base-input': '#EDEDED',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-title': '#272221',
+
+  purple: '#8047F8',
+  'purple-dark': '#4B2995',
+  'purple-light': '#EBE5F9',
+
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
+  'yellow-light': '#F1E9C9',
+}
